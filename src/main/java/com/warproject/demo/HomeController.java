23162314a9396b592ff996e070e8f79985534750
santhoshrst <rsanthoshrst@gmail.com>
2025-12-16 17:21:45 +0530

    Update HomeController.java

--- src/main/java/com/warproject/demo/HomeController.java
+++ src/main/java/com/warproject/demo/HomeController.java
@@ -1,12 +1,14 @@
 package com.warproject.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("home")
+    @GetMapping
     public String home() {
         return "NEW WAR WORKING";
     }
