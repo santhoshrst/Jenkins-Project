1ae0f5916be0351ead3662ae2b40862783293509
santhoshrst <rsanthoshrst@gmail.com>
2025-12-16 15:16:16 +0530

    jenkin test

--- src/main/java/com/warproject/demo/HomeController.java
+++ src/main/java/com/warproject/demo/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ControllerClass {
+public class HomeController {
 
     @GetMapping("/home")
-    public String home(){
-        return "Hello World";
+    public String home() {
+        return "WAR WORKING";
     }
-
 }
--- src/main/java/com/warproject/demo/WarprojectApplication.java
+++ src/main/java/com/warproject/demo/WarprojectApplication.java
@@ -2,12 +2,18 @@ package com.warproject.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarprojectApplication {
+public class WarprojectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarprojectApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WarprojectApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(WarprojectApplication.class, args);
+    }
 }
